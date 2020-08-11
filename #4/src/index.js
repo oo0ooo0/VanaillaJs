97dfeabc9681219e0432d388bdb9792926ddfa4e
jamin-coo <jamin@cooljamm.com>
2020-08-12 08:40:49 +0900

    day4

--- #4/src/index.js
+++ #4/src/index.js
@@ -1,3 +1,41 @@
 // <⚠️ DONT DELETE THIS ⚠️>
-import "./styles.css";
+// import './styles.css';
 // <⚠️ /DONT DELETE THIS ⚠️>
+// const colors = [
+//   'linear-gradient(to top, #b8cbb8 0%, #b8cbb8 0%, #b465da 0%, #cf6cc9 33%, #ee609c 66%, #ee609c 100%)',
+//   'linear-gradient(120deg, #72afd3 0%, #068024 100%)',
+//   'linear-gradient(to top, #37ecba 0%, #72afd3 100%)',
+//   'linear-gradient(-20deg, #6e45e2 0%, #88d3ce 100%)',
+//   'linear-gradient(180deg, #5af778 0%, #009efd 100%)',
+// ];
+
+// const body = document.querySelector('body');
+
+// const superEventHandler = {
+//   resize: function handleResize() {
+//     let wide = window.innerWidth;
+//     console.log(wide);
+//     if (wide < 300) {
+//       body.style.background = colors[0];
+//     } else if (wide < 500) {
+//       body.style.background = colors[1];
+//     } else if (wide < 700) {
+//       body.style.background = colors[2];
+//     } else if (wide < 900) {
+//       body.style.background = colors[3];
+//     } else {
+//       body.style.background = colors[4];
+//     }
+//   },
+// };
+
+// window.addEventListener('resize', superEventHandler.resize);
+
+//템플릿 리터럴
+/* function aa(a,b){
+   return `Hello ${a}, you are not good ${b}.`;
+ }
+
+const greet = aa('sick','man')
+
+ console.log(greet)*/
