8e6fca4c76a58f9bb912fbafcf88ceaaeeca9d54
oo0ooo0 <nyanyanyangnyang@gmail.com>
2021-01-16 18:03:35 +0900

    add todo list

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const clockContainer = document.querySelector('.js-clock'),
 
 function getTime() {
   const date = new Date();
-  const h = date.getHours();
-  const m = date.getMinutes();
-  const s = date.getSeconds();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
 
-  clockTitle.innerText = `${h < 10 ? `0${h}` : h}:${m < 10 ? `0${m}` : m}:${s < 10 ? `0${s}` : s}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
 }
 
 function init() {
